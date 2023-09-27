1b4a8775adf09d92b5de77511aa2ae16ffc4c7e8
Tan Justinn <ohhoehoe@gmail.com>
2023-09-27 12:27:54 +0800

    Starter Springboot code

---
+++ backend/src/main/java/com/septismjustinn/fizzbuzzgen/FizzbuzzgenApplication.java
@@ -0,0 +1,13 @@
+package com.septismjustinn.fizzbuzzgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzbuzzgenApplication.class, args);
+	}
+
+}
